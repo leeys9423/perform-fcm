84463df416fb76d47530ad3851183ee07ac3d790
leeys9423 <leeys9423@gmail.com>
2025-03-15 18:19:29 +0900

    test: TestConfig 파일 생성
    - .env 경로 변경

--- src/main/java/com/example/fcm/FcmApplication.java
+++ src/main/java/com/example/fcm/FcmApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:.env")
 public class FcmApplication {
 
     public static void main(String[] args) {
